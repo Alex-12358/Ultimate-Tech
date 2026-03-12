b16149dd376b184ba9b35cb904aae15d27c0a5fe
Alexei Tarba <141357310+Alex-12358@users.noreply.github.com>
2026-03-12 20:17:34 +0300

    miniUpdate1.2

--- src/main/java/org/mod/ultimate_tech/datagen/ModBlockTagGenerator.java
+++ src/main/java/org/mod/ultimate_tech/datagen/ModBlockTagGenerator.java
@@ -2,14 +2,14 @@ package org.mod.ultimate_tech.datagen;
 
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
+import net.minecraft.tags.BlockTags;
+import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.data.BlockTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
 import org.mod.ultimate_tech.Ultimate_tech;
-import org.mod.ultimate_tech.block.ModBlocksBlock;
-import org.mod.ultimate_tech.block.ModBlocksDeepslateOre;
-import org.mod.ultimate_tech.block.ModBlocksOre;
-import org.mod.ultimate_tech.block.ModBlocksRaw;
+import org.mod.ultimate_tech.block.*;
+import org.mod.ultimate_tech.block.custom.ModBlocks;
 import org.mod.ultimate_tech.util.ModTags;
 
 import java.util.concurrent.CompletableFuture;
@@ -22,32 +22,100 @@ public class ModBlockTagGenerator extends BlockTagsProvider {
     @Override
     protected void addTags(HolderLookup.Provider pProvider) {
 
-        this.tag()
-
+        // =========================
         // CORE BLOCKS
+        // =========================
+
         this.tag(ModTags.Blocks.CORE_STRUCTURAL_BLOCK)
-                .add(ModBlocksOre.CORE_STRUCTURAL_BLOCK.get());
-//        this.tag(ModTags.Blocks.MACHINE_STRUCTURAL_HOUSING)
-//                .add(ModBlocks.MACHINE_STRUCTURAL_HOUSING.get());
+                .add(ModBlocks.CORE_STRUCTURAL_BLOCK.get());
 
-        // ORES
-        ModBlocksOre.ORES.values().forEach(block ->
-                tag(ModTags.Blocks.ORES).add(block.get())
-        );
+        this.tag(BlockTags.MINEABLE_WITH_PICKAXE)
+                .add(ModBlocks.CORE_STRUCTURAL_BLOCK.get());
 
+        this.tag(BlockTags.NEEDS_IRON_TOOL)
+                .add(ModBlocks.CORE_STRUCTURAL_BLOCK.get());
+
+        // =========================
+        // OVERWORLD ORES
+        // =========================
+
+        this.tag(ModTags.Blocks.ORES)
+                .addTag(Tags.Blocks.ORES);
+
+        ModBlocksOre.ORES.values().forEach(block -> {
+            this.tag(ModTags.Blocks.ORES).add(block.get());
+
+            this.tag(BlockTags.MINEABLE_WITH_PICKAXE).add(block.get());
+            this.tag(BlockTags.NEEDS_IRON_TOOL).add(block.get());
+        });
+
+        // =========================
         // DEEPSLATE ORES
-        ModBlocksDeepslateOre.ORES.values().forEach(block ->
-                tag(ModTags.Blocks.DEEPSLATE_ORES).add(block.get())
-        );
+        // =========================
 
+        this.tag(ModTags.Blocks.DEEPSLATE_ORES)
+                .addTag(Tags.Blocks.ORES);
+
+        ModBlocksDeepslateOre.ORES.values().forEach(block -> {
+            this.tag(ModTags.Blocks.DEEPSLATE_ORES).add(block.get());
+
+            this.tag(BlockTags.MINEABLE_WITH_PICKAXE).add(block.get());
+            this.tag(BlockTags.NEEDS_IRON_TOOL).add(block.get());
+        });
+
+
+        // =========================
+        // NETHER ORES
+        // =========================
+
+        this.tag(ModTags.Blocks.NETHER_ORES)
+                .addTag(Tags.Blocks.ORES);
+
+        ModBlocksNetherOre.ORES.values().forEach(block -> {
+            this.tag(ModTags.Blocks.NETHER_ORES).add(block.get());
+
+            this.tag(BlockTags.MINEABLE_WITH_PICKAXE).add(block.get());
+            this.tag(BlockTags.NEEDS_IRON_TOOL).add(block.get());
+        });
+
+
+        // =========================
+        // END ORES
+        // =========================
+
+        this.tag(ModTags.Blocks.END_ORES)
+                .addTag(Tags.Blocks.ORES);
+
+        ModBlocksEndOre.ORES.values().forEach(block -> {
+            this.tag(ModTags.Blocks.END_ORES).add(block.get());
+
+            this.tag(BlockTags.MINEABLE_WITH_PICKAXE).add(block.get());
+            this.tag(BlockTags.NEEDS_IRON_TOOL).add(block.get());
+        });
+
+        // =========================
         // RAW BLOCKS
-        ModBlocksRaw.RAW_BLOCKS.values().forEach(block ->
-                tag(ModTags.Blocks.RAW_BLOCKS).add(block.get())
-        );
+        // =========================
 
+        ModBlocksRaw.RAW_BLOCKS.values().forEach(block -> {
+            this.tag(ModTags.Blocks.RAW_BLOCKS).add(block.get());
+
+            this.tag(BlockTags.MINEABLE_WITH_PICKAXE).add(block.get());
+            this.tag(BlockTags.NEEDS_IRON_TOOL).add(block.get());
+        });
+
+        // =========================
         // STORAGE BLOCKS
-        ModBlocksBlock.BLOCK.values().forEach(block ->
-                tag(ModTags.Blocks.STORAGE_BLOCKS).add(block.get())
-        );
+        // =========================
+
+        this.tag(ModTags.Blocks.STORAGE_BLOCKS)
+                .addTag(Tags.Blocks.STORAGE_BLOCKS);
+
+        ModBlocksBlock.BLOCK.values().forEach(block -> {
+            this.tag(ModTags.Blocks.STORAGE_BLOCKS).add(block.get());
+
+            this.tag(BlockTags.MINEABLE_WITH_PICKAXE).add(block.get());
+            this.tag(BlockTags.NEEDS_IRON_TOOL).add(block.get());
+        });
     }
 }
--- src/main/java/org/mod/ultimate_tech/datagen/loot/ModBlockLootTables.java
+++ src/main/java/org/mod/ultimate_tech/datagen/loot/ModBlockLootTables.java
@@ -1,4 +1,106 @@
 package org.mod.ultimate_tech.datagen.loot;
 
-public class ModBlockLootTables {
+import net.minecraft.data.loot.BlockLootSubProvider;
+import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.ApplyBonusCount;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
+import net.minecraftforge.registries.RegistryObject;
+import org.mod.ultimate_tech.block.*;
+import org.mod.ultimate_tech.block.custom.*;
+import org.mod.ultimate_tech.item.*;
+
+import java.util.Set;
+
+public class ModBlockLootTables extends BlockLootSubProvider {
+
+    // Сделать конструктор public
+    public ModBlockLootTables() {
+        super(Set.of(), FeatureFlags.REGISTRY.allFlags());
+    }
+
+    @Override
+    protected void generate() {
+
+        // =========================
+        // SIMPLE BLOCKS
+        // =========================
+
+        this.dropSelf(ModBlocks.CORE_STRUCTURAL_BLOCK.get());
+        this.dropSelf(ModBlocks.MACHINE_STRUCTURAL_HOUSING.get());
+
+        // =========================
+        // OVERWORLD ORES
+        // =========================
+
+        ModBlocksOre.ORES.forEach((material, block) -> {
+            if (!ModItemsRaw.RAW_ITEMS.containsKey(material)) return;
+
+            this.add(block.get(), b ->
+                    createFortuneOreDrops(
+                            block.get(),
+                            ModItemsRaw.RAW_ITEMS.get(material).get()
+                    ));
+        });
+
+        // =========================
+        // DEEPSLATE ORES
+        // =========================
+
+        ModBlocksDeepslateOre.ORES.forEach((material, block) -> {
+            if (!ModItemsRaw.RAW_ITEMS.containsKey(material)) return;
+
+            this.add(block.get(), b ->
+                    createFortuneOreDrops(
+                            block.get(),
+                            ModItemsRaw.RAW_ITEMS.get(material).get()
+                    ));
+        });
+
+        // =========================
+        // NETHER ORES
+        // =========================
+
+        ModBlocksNetherOre.ORES.forEach((material, block) -> {
+            if (!ModItemsRaw.RAW_ITEMS.containsKey(material)) return;
+
+            this.add(block.get(), b ->
+                    createFortuneOreDrops(
+                            block.get(),
+                            ModItemsRaw.RAW_ITEMS.get(material).get()
+                    ));
+        });
+
+        // =========================
+        // END ORES
+        // =========================
+
+        ModBlocksEndOre.ORES.forEach((material, block) -> {
+            if (!ModItemsRaw.RAW_ITEMS.containsKey(material)) return;
+
+            this.add(block.get(), b ->
+                    createFortuneOreDrops(
+                            block.get(),
+                            ModItemsRaw.RAW_ITEMS.get(material).get()
+                    ));
+        });
+    }
+
+    protected LootTable.Builder createFortuneOreDrops(Block pBlock, Item item) {
+        return createSilkTouchDispatchTable(pBlock,
+                this.applyExplosionDecay(pBlock,
+                        LootItem.lootTableItem(item)
+                                .apply(SetItemCountFunction.setCount(UniformGenerator.between(2.0F, 5.0F)))
+                                .apply(ApplyBonusCount.addOreBonusCount(Enchantments.BLOCK_FORTUNE))));
+    }
+
+    @Override
+    protected Iterable<Block> getKnownBlocks() {
+        return ModBlockUtils.BLOCKS.getEntries().stream().map(RegistryObject::get)::iterator;
+    }
 }
